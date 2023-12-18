9ed30c32012ec55ea46908b1d6eaa1d2a85b05a9
Gauransh Juneja <gauranshjuneja@gmail.com>
2023-12-18 10:11:50 +0530

    second commit

--- app/api/route.ts
+++ app/api/route.ts
@@ -5,7 +5,7 @@ let data = { record: { email: "test12@gmail.com" } }
 export async function GET(request: Request) {
     return NextResponse.json({
         status: 200,
-        data
+        last_user:data.record.email || null
     })
 }
 
