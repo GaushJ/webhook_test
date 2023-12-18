125f9b8616141e0ef80760dc78ace50c6d79a8ba
Gauransh Juneja <gauranshjuneja@gmail.com>
2023-12-18 09:44:38 +0530

    first commit

---
+++ app/api/route.ts
@@ -0,0 +1,22 @@
+import { NextResponse } from "next/server";
+
+let data = { record: { email: "test12@gmail.com" } }
+
+export async function GET(request: Request) {
+    return NextResponse.json({
+        status: 200,
+        data
+    })
+}
+
+export async function POST(request: Request) {
+    const req = await request.json();
+
+    data = req;
+
+    return NextResponse.json({
+        status: 200,
+        data,
+        message: "Data has been updated"
+    })
+}
